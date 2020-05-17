d104f3afa2da05fe75d8fc17e2d7898a3753940c
mr <moriryosuke48@gmail.com>
2020-05-17 15:18:11 +0900

    add functions relating correlation

--- automate_tool.py
+++ automate_tool.py
@@ -9,6 +9,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import scipy.stats as st
 
 def is_categorical(data, key):
     
@@ -85,3 +86,71 @@ def summarize_data(df):
     pd.set_option('display.max_rows', len(df.keys()))
     
     return df_summary
+
+def correlation_ratio(cat_key, num_key, data):
+    
+    categorical=data[cat_key]
+    numerical=data[num_key]
+    
+    mean=numerical.dropna().mean()
+    all_var=((numerical-mean)**2).sum()
+    
+    unique_cat=pd.Series(categorical.unique())
+    unique_cat=list(unique_cat.dropna())
+    
+    categorical_num=[numerical[categorical==cat] for cat in unique_cat]
+    categorical_var=[len(x.dropna())*(x.dropna().mean()-mean)**2 for x in categorical_num]    
+
+    r=sum(categorical_var)/all_var
+    
+    return r
+
+def cramerV(x, y, data):
+    
+    table=pd.crosstab(data[x], data[y])
+    x2, p, dof, e=st.chi2_contingency(table, False)
+    
+    n=table.sum().sum()
+    r=x2/(n*(np.min(table.shape)-1))
+
+    return r
+
+def get_corr(data, categorical_keys=None):
+    
+    keys=data.keys()
+        
+    if categorical_keys is None:
+        
+        categorical_keys=keys[[is_categorical(data, key) for key in keys]]
+        
+    corr=pd.DataFrame({})
+    corr_ratio=pd.DataFrame({})
+    corr_cramer=pd.DataFrame({})
+        
+    try:
+        for key1 in keys:
+            for key2 in keys:
+
+                if (key1 in categorical_keys) and (key2 in categorical_keys):
+
+                    r=cramerV(key1, key2, data)
+                    corr_cramer.loc[key1, key2]=r                
+
+                elif (key1 in categorical_keys) and (key2 not in categorical_keys):
+
+                    r=correlation_ratio(cat_key=key1, num_key=key2, data=data)
+                    corr_ratio.loc[key1, key2]=r                
+
+                elif (key1 not in categorical_keys) and (key2 in categorical_keys):
+
+                    r=correlation_ratio(cat_key=key2, num_key=key1, data=data)
+                    corr_ratio.loc[key1, key2]=r                
+
+                else:
+
+                    r=data.corr().loc[key1, key2]
+                    corr.loc[key1, key2]=r                
+    except:
+        print(key1, key2)
+            
+    return corr, corr_ratio, corr_cramer
